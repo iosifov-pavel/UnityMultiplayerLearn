8158b1c29919268f0c6e800f2012b96dcd549274
91i4ch <iosifov.p.v@gmail.com>
2021-07-25 18:35:44 +0300

    Networking 9/50

--- Assets/Scripts/MyNetworkPlayer.cs
+++ Assets/Scripts/MyNetworkPlayer.cs
@@ -1,19 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using Mirror;
+using TMPro;
 using UnityEngine;
 
 public class MyNetworkPlayer : NetworkBehaviour
 {
-    [SyncVar][SerializeField] string displayName ="Missing Name"; 
-    [SyncVar][SerializeField] Color playerColor = Color.white;
+    [SyncVar(hook =nameof(HandleUINameUpdated))][SerializeField] string displayName ="Missing Name"; 
+    [SyncVar(hook=nameof(HandleDisplayColourUpdated))][SerializeField] Color playerColor = Color.white;
+    [SerializeField] TMP_Text playerNameUI = null;
+    [SerializeField] Renderer displayColorRender = null;
+    #region Server
     // Start is called before the first frame update
     [Server]
     public void SetDisplayName(string newName){
         displayName = newName;
     }
-
+    [Server]
     public void SetPlayerColor(Color newColor){
         playerColor = newColor;
     }
+    [Command]
+    private void CmdSetDisplayName(string newDisplayName){
+        RpcLogNewName(newDisplayName); 
+        SetDisplayName(newDisplayName);
+    }
+    #endregion
+    #region Client
+    private void HandleDisplayColourUpdated(Color oldColor, Color newColor){
+        displayColorRender.material.SetColor("_BaseColor", newColor);
+    }
+
+    private void HandleUINameUpdated(string oldName, string newName){
+        playerNameUI.text = newName;
+    }
+
+    [ContextMenu("SetMyName")]
+    private void SetMyName(){
+        CmdSetDisplayName("My New Name");
+    }
+
+    [ClientRpc]
+    private void RpcLogNewName(string newName){
+        Debug.Log(newName);
+    }
+    #endregion
+
+
 }
